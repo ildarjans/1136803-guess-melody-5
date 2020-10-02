10c5778e64ff209d613c897cb28d6babbc3bfa4e
ildarjan <nobap84@rambler.ru>
2020-10-02 18:59:37 +0300

    Add PropTypes and Welcome compopent

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const Settings = {
+  ERROR_COUNT: 3,
+};
+
+const root = document.querySelector(`#root`);
+
+ReactDOM.render(
+    <App errorsCount={Settings.ERROR_COUNT}/>,
+    root
+);
 
