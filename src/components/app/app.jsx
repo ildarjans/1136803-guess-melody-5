23cdcf1c098b278cff99708f6c08ad7671d473b8
ildarjan <nobap84@rambler.ru>
2020-10-04 22:45:45 +0300

    add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import LoginScreen from "../login-screen/login-screen";
+import ResultLoseScreen from "../result-lose-screen/result-lose-screen";
+import ResultWinScreen from "../result-win-screen/result-win-screen";
+import QuestionArtistScreen from "../question-artist-screen/question-artist-screen";
+import QuestionGenreScreen from "../question-genre-screen/question-genre-screen";
 
 
 const App = (props) => {
   const {errorsCount} = props;
-
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount}/>
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen/>
+        </Route>
+        <Route exact path="/result">
+          <ResultWinScreen/>
+        </Route>
+        <Route exact path="/lose">
+          <ResultLoseScreen/>
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenreScreen/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtistScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
