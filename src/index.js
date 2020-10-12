b82a15132e63dc14c12b6cbc014d94d5938eec8b
ildarjan <nobap84@rambler.ru>
2020-10-13 02:18:17 +0300

    set named imports. Remove ErrorCount const

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app/app';
-import {Settings} from "./const";
-import questions from "./mocks/questions";
+import {questions} from "./mocks/questions";
 
 const root = document.querySelector(`#root`);
 
 ReactDOM.render(
     <App
-      errorsCount={Settings.ERROR_COUNT}
       questions={questions}
     />,
     root
