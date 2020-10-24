a69d8e9b8a2427ebec8c34a90f2db1b1ee754684
ildarjan <nobap84@rambler.ru>
2020-10-25 01:50:42 +0300

    add to export HOCed component

--- src/components/game-artist/game-artist.jsx
+++ src/components/game-artist/game-artist.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
+import {withAudioPlayer} from "../../hocs/with-audio-player";
+import {CircleStyle} from "../../styles/circle-style";
 import {ArtistInput} from "../artist-input/artist-input";
 import {ArtistQuestionPropTypes} from "../artist-prop-types/artist-question";
-import {CircleStyle} from "../../styles/circle-style";
 
-export const GameArtist = ({onAnswer, question, renderPlayer}) => {
+
+export const GameArtist = ({onAnswer, question, renderPlayer, children}) => {
   return (
     <section className="game game--artist">
       <header className="game__header">
@@ -23,11 +25,8 @@ export const GameArtist = ({onAnswer, question, renderPlayer}) => {
             style={CircleStyle}/>
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong"/>
-          <div className="wrong"/>
-          <div className="wrong"/>
-        </div>
+        {children}
+
       </header>
 
       <section className="game__screen">
@@ -63,4 +62,7 @@ GameArtist.propTypes = {
   renderPlayer: PropTypes.func.isRequired,
   onAnswer: PropTypes.func.isRequired,
   question: ArtistQuestionPropTypes.isRequired,
+  children: PropTypes.element.isRequired
 };
+
+export const GameArtistWithAudioPlayer = withAudioPlayer(GameArtist);
