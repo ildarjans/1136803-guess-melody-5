6d307dc8921e7e2174339362c44992d1ee928966
ildarjans <nobap84@rambler.ru>
2020-11-01 00:57:26 +0300

    refactor resetGame

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,6 @@
 import {ActionType} from "./action-type";
 import {questions} from "../mocks/questions";
 import {extend} from "../utils";
-import {MAX_ERROR_COUNT} from "../const";
 
 const initialState = {
   mistakes: 0,
@@ -12,8 +11,7 @@ const initialState = {
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case ActionType.INCREMENT_MISTAKES:
-      const mistakes = state.mistakes + action.payload;
-      return mistakes >= MAX_ERROR_COUNT ? extend({}, initialState) : extend(state, {mistakes});
+      return extend(state, {mistakes: state.mistakes + action.payload});
     case ActionType.INCREMENT_STEPS:
       return extend(state, {
         step: state.step + action.payload
